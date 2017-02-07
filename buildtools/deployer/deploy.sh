bcbee92eac24c029cac9a74d87cf05ca8f464766
Jejomar Dimayuga <jdimayuga@exist.com>
2017-02-07 21:50:18 +0800

    added dev profile for automated deployment

--- buildtools/deployer/deploy.sh
+++ buildtools/deployer/deploy.sh
@@ -1,3 +1,3 @@
-echo 'Deploying: '$1
-echo curl -vX POST http://pemc-dcos.southeastasia.cloudapp.azure.com/marathon/v2/apps?force=true -d @$1 --header "Content-Type: application/json"
-curl -vX POST http://pemc-dcos.southeastasia.cloudapp.azure.com/marathon/v2/apps?force=true -d @$1 --header "Content-Type: application/json"
+echo 'Deploying: '$1' '$2
+echo curl -vX POST $1force=true -d @$2 --header "Content-Type: application/json"
+#curl -vX POST $1?force=true -d @$2 --header "Content-Type: application/json"

31937dc22b272f52d336202818707c2c02938f4a
mlnicolas <mlnicolas@exist.com>
2017-02-28 19:51:42 +0800

    fixed prelim/final validation

--- src/main/java/com/pemc/crss/dataflow/app/service/impl/TaskExecutionServiceImpl.java
+++ src/main/java/com/pemc/crss/dataflow/app/service/impl/TaskExecutionServiceImpl.java
@@ -231,13 +231,13 @@ public class TaskExecutionServiceImpl implements TaskExecutionService {
                 properties.add(concatKeyValue(SPRING_PROFILES_ACTIVE, fetchSpringProfilesActive("dailyMq")));
             } else {
                 String processType = taskRunDto.getMeterProcessType();
-                    /*if (!processType.equalsIgnoreCase(MeterProcessType.PRELIM.name())) {
-                        String processBefore = processType.equalsIgnoreCase(MeterProcessType.FINAL.name()) ?
-                                MeterProcessType.PRELIM.name() : MeterProcessType.FINAL.name();
-                        String errMsq = "Must run " + processBefore + " first!";
-                        Preconditions.checkState(executionParamRepository.countMonthlyRun(taskRunDto.getStartDate(),
-                                taskRunDto.getEndDate(), processBefore) == 0, errMsq);
-                    }*/
+                if (!processType.equalsIgnoreCase(MeterProcessType.PRELIM.name())) {
+                    String processBefore = processType.equalsIgnoreCase(MeterProcessType.FINAL.name()) ?
+                            MeterProcessType.PRELIM.name() : MeterProcessType.FINAL.name();
+                    String errMsq = "Must run " + processBefore + " first!";
+                    Preconditions.checkState(executionParamRepository.countMonthlyRun(taskRunDto.getStartDate(),
+                            taskRunDto.getEndDate(), processBefore) > 0, errMsq);
+                }
                 arguments.add(concatKeyValue(START_DATE, taskRunDto.getStartDate(), "date"));
                 arguments.add(concatKeyValue(END_DATE, taskRunDto.getEndDate(), "date"));
                 arguments.add(concatKeyValue(PROCESS_TYPE, processType));

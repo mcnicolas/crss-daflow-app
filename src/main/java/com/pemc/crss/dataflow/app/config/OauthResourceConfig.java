47729f52e09c418fc05eb0ee0b57ca261de77fa1
Nas Villanueva <jvillanueva@exist.com>
2017-03-02 12:09:33 +0800

    Added authentication for UI and dataflow-app
    (cherry picked from commit ef23eb5)

---
+++ src/main/java/com/pemc/crss/dataflow/app/config/OauthResourceConfig.java
@@ -0,0 +1,11 @@
+package com.pemc.crss.dataflow.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@Configuration
+@EnableResourceServer
+@EnableOAuth2Client
+public class OauthResourceConfig {
+}
---
+++ src/main/java/com/pemc/crss/dataflow/app/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.pemc.crss.dataflow.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
+
+@Configuration
+@Import({OauthResourceConfig.class, CORSFilter.class, SecurityConfig.MethodSecurityConfiguration.class})
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .anyRequest().authenticated();
+    }
+
+    @Configuration
+    @EnableGlobalMethodSecurity(prePostEnabled = true)
+    public static class MethodSecurityConfiguration extends GlobalMethodSecurityConfiguration {
+
+        @Override
+        protected MethodSecurityExpressionHandler createExpressionHandler() {
+            return new OAuth2MethodSecurityExpressionHandler();
+        }
+    }
+
+
+}

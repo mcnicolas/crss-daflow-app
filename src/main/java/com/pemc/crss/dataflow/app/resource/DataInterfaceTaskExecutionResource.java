c650a79f1e630ad31fd6df2601ce99087d866157
mlnicolas <mlnicolas@exist.com>
2017-03-20 15:55:15 +0800

    temp comment this

--- src/main/java/com/pemc/crss/dataflow/app/resource/DataInterfaceTaskExecutionResource.java
+++ src/main/java/com/pemc/crss/dataflow/app/resource/DataInterfaceTaskExecutionResource.java
@@ -34,8 +34,9 @@ public class DataInterfaceTaskExecutionResource {
             @RequestParam String runStartDate, @RequestParam String runEndDate, @RequestParam String tradingStartDate,
             @RequestParam String tradingEndDate) {
         LOG.debug("Finding job instances request. pageable={}", pageable);
-        return new ResponseEntity<>(taskExecutionService.findJobInstances(pageable, type, status, mode,
-                runStartDate, runEndDate, tradingStartDate, tradingEndDate), HttpStatus.OK);
+        /*return new ResponseEntity<>(taskExecutionService.findJobInstances(pageable, type, status, mode,
+                runStartDate, runEndDate, tradingStartDate, tradingEndDate), HttpStatus.OK);*/
+        return new ResponseEntity<Page<? extends BaseTaskExecutionDto>>(HttpStatus.OK);
     }
 
     @RequestMapping(method = RequestMethod.POST)

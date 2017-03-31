3af94cc3fed42facd5ec87c437a6fcc5bdbdb2ad
mlnicolas <mlnicolas@exist.com>
2017-03-31 19:20:34 +0800

    removed enableSecurity profile

--- src/main/java/com/pemc/crss/dataflow/app/config/CORSFilter.java
+++ src/main/java/com/pemc/crss/dataflow/app/config/CORSFilter.java
@@ -1,6 +1,5 @@
 package com.pemc.crss.dataflow.app.config;
 
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -10,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Profile("enableSecurity")
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CORSFilter implements Filter {
--- src/main/java/com/pemc/crss/dataflow/app/config/OauthResourceConfig.java
+++ src/main/java/com/pemc/crss/dataflow/app/config/OauthResourceConfig.java
@@ -1,11 +1,9 @@
 package com.pemc.crss.dataflow.app.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
-@Profile("enableSecurity")
 @Configuration
 @EnableResourceServer
 @EnableOAuth2Client

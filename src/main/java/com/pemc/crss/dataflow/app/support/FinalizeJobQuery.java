c0503620ab031ed368c2dc77eb0467ae507052bf
KAcejo <kacejo@exist.com>
2017-04-27 11:20:09 +0800

    Fix for 152460.

--- src/main/java/com/pemc/crss/dataflow/app/support/FinalizeJobQuery.java
+++ src/main/java/com/pemc/crss/dataflow/app/support/FinalizeJobQuery.java
@@ -6,7 +6,7 @@ public class FinalizeJobQuery {
     public static final String FINALIZE_JOB_NAME_FINAL = "tasAsOutputReadyMonthlyFinal%";
 
     public static String countQuery() {
-        return "SELECT count(*) FROM batch_job_instance ji JOIN batch_job_execution je ON ji.job_instance_id = je.job_instance_id JOIN batch_job_execution_params jep ON je.job_execution_id = jep.job_execution_id WHERE ji.job_name like ? AND je.status = 'COMPLETED' AND (jep.KEY_NAME = 'startDate' AND TO_CHAR(jep.DATE_VAL, 'MM/DD/YYYY') LIKE ?) AND (jep.KEY_NAME = 'endDate' AND TO_CHAR(jep.DATE_VAL, 'MM/DD/YYYY') LIKE ?) AND (jep.KEY_NAME = 'processType' AND jep.STRING_VAL = ?) ORDER BY JE.START_TIME DESC";
+        return "SELECT count(*) FROM batch_job_instance ji JOIN batch_job_execution je ON ji.job_instance_id = je.job_instance_id JOIN batch_job_execution_params jep1 ON je.job_execution_id = jep1.job_execution_id JOIN batch_job_execution_params jep2 ON je.job_execution_id = jep2.job_execution_id JOIN batch_job_execution_params jep3 ON je.job_execution_id = jep3.job_execution_id WHERE ji.job_name like ? AND je.status = 'COMPLETED' AND (jep1.KEY_NAME = 'startDate' AND TO_CHAR(jep1.DATE_VAL, 'YYYY-MM-DD') LIKE ?) AND (jep2.KEY_NAME = 'endDate' AND TO_CHAR(jep2.DATE_VAL, 'YYYY-MM-DD') LIKE ?) AND (jep3.KEY_NAME = 'processType' AND jep3.STRING_VAL = ?)";
     }
 
 }

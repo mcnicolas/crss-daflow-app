f9cf309c2d330aab1466bdfa9435eb67fab5e9e9
dmendoza <dmendoza@exist.com>
2018-06-28 10:08:14 +0800

    fix bug#174221 Simultaneous Job Run : Monthly GEN MTR skipped in job queue for monthly

--- src/main/java/com/pemc/crss/dataflow/app/jobqueue/SchedulerService.java
+++ src/main/java/com/pemc/crss/dataflow/app/jobqueue/SchedulerService.java
@@ -1,13 +1,11 @@
 package com.pemc.crss.dataflow.app.jobqueue;
 
 import com.pemc.crss.shared.commons.reference.MeterProcessType;
-import com.pemc.crss.shared.core.dataflow.reference.JobProcess;
 import com.pemc.crss.shared.core.dataflow.reference.QueueStatus;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static com.pemc.crss.shared.core.dataflow.reference.JobProcess.*;
 import static com.pemc.crss.shared.core.dataflow.reference.QueueStatus.*;
 
 public interface SchedulerService {
@@ -20,7 +18,7 @@ public interface SchedulerService {
     List<MeterProcessType> DAILY_AND_AC = Arrays.asList(MeterProcessType.DAILY, MeterProcessType.AC, MeterProcessType.DEFAULT);
 
     List<MeterProcessType> MONTHLY = Arrays.asList(MeterProcessType.PRELIM, MeterProcessType.PRELIMINARY,
-            MeterProcessType.FINAL, MeterProcessType.ADJUSTED);
+            MeterProcessType.FINAL, MeterProcessType.ADJUSTED, MeterProcessType.MONTHLY);
 
     List<QueueStatus> IN_PROGRESS_STATUS = Arrays.asList(ON_QUEUE, STARTED, STARTING);
 }

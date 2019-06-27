f5fec2809cd7ca7823af3f0efa660d59f618cd9f
Joshua Mengote <jmengote@exist.com>
2019-06-27 19:22:37 +0800

    [190336] Add GEN_RESERVE_BILLING_STATEMENTS to runNextQueuedJob(...)

--- src/main/java/com/pemc/crss/dataflow/app/jobqueue/impl/SchedulerServiceImpl.java
+++ src/main/java/com/pemc/crss/dataflow/app/jobqueue/impl/SchedulerServiceImpl.java
@@ -219,6 +219,7 @@ public class SchedulerServiceImpl implements SchedulerService {
                 case GEN_LR_FILES:
                 case STL_VALIDATION:
                 case GEN_ENERGY_BILLING_STATEMENTS:
+                case GEN_RESERVE_BILLING_STATEMENTS:
                     tradingAmountsTaskExecutionService.launchJob(taskDto);
                     break;
                 case GEN_INPUT_WS_EMF:

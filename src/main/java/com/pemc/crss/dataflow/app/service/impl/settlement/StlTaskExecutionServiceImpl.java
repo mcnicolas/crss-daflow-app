d844ec908fd1a8585a894f8a101edd43025b11d6
Joshua Mengote <jmengote@exist.com>
2019-11-19 17:59:04 +0800

    modify logs for debugging

--- src/main/java/com/pemc/crss/dataflow/app/service/impl/settlement/StlTaskExecutionServiceImpl.java
+++ src/main/java/com/pemc/crss/dataflow/app/service/impl/settlement/StlTaskExecutionServiceImpl.java
@@ -400,10 +400,8 @@ public abstract class StlTaskExecutionServiceImpl extends AbstractTaskExecutionS
 
             stlJobGroupDtoMap.put(groupId, stlJobGroupDto);
 
-            if (genFileJobInstance.getInstanceId() == 20992) {
-                log.info("generationJobExecution: id={}", generationJobExecution.getId());
-                log.info("generationJobExecution: execution_context={}", generationJobExecution.getExecutionContext().toString());
-            }
+            log.info("generationJobExecution: id={}", generationJobExecution.getId());
+            log.info("generationJobExecution: execution_context={}", generationJobExecution.getExecutionContext().toString());
 
             Optional.ofNullable(generationJobExecution.getExecutionContext()
                     .get(INVOICE_GENERATION_FILENAME_KEY)).ifPresent(val ->

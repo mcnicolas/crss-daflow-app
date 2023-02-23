a0db4cae3052e3f339fae142e622ab53585c7458
Julius Lapugot <jlapugot@exist.com>
2023-02-23 09:06:23 +0800

    [272807] Change step key for calculate rta

--- src/main/java/com/pemc/crss/dataflow/app/service/impl/settlement/ReserveTradingAmountsTaskExecutionServiceImpl.java
+++ src/main/java/com/pemc/crss/dataflow/app/service/impl/settlement/ReserveTradingAmountsTaskExecutionServiceImpl.java
@@ -503,7 +503,7 @@ public class ReserveTradingAmountsTaskExecutionServiceImpl extends StlTaskExecut
         Map<String, String> calcSteps = new LinkedHashMap<>();
         calcSteps.put(DISAGGREGATE_BCQ, "Disaggregate BCQ");
         calcSteps.put(CALC_SCALING_FACTOR, "Calculate Scaling Factor");
-        calcSteps.put(CALC_RCRA, "Calculate RTA");
+        calcSteps.put(CALC_RTA, "Calculate RTA");
 
         return calcSteps;
     }
